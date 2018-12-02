0f62822c64ba820a2bf9188ca10554491efa901e
Paul Ashour <paul.ashour@gmail.com>
2018-12-02 01:06:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  
+  attr_reader :side_a, :side_b, :side_c, :kind
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+    @kind = self.class.determine_kind(side_a, side_b, side_c)
+  end
+  
+  def self.determine_kind(a, b, c)
+    sides = [a, b, c].sort
+    if !(sides[2] < sides[0] + sides[1] && sides[0] > 0)
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+    [:equilateral, :isosceles, :scalene][sides.uniq.count - 1]
+  end
+  
+  class TriangleError < StandardError
+    
+  end
+      
+  
 end
